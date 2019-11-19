b44192952ad7901911da75daa90ee66cc567f1fc
nomad23541 <chrisreading.email@gmail.com>
2019-11-18 19:20:29 -0700

    Deleted TestClass.java fixed CountdownTimer

--- src/wordscrambler/CountdownTimer.java
+++ src/wordscrambler/CountdownTimer.java
@@ -11,7 +11,6 @@ public class CountdownTimer {
 	
 	public CountdownTimer(int countdown) {
 		this.countdown = countdown;
-		
 	}
 	
 }
--- src/wordscrambler/TestClass.java
+++
@@ -1,9 +0,0 @@
-package wordscrambler;
-
-public class TestClass {
-	
-	public TestClass() {
-		System.out.println("test");
-	}
-
-}
