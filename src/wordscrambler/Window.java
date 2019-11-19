e95263fca48de252352c8d3a05d1426d03a18d0f
nomad23541 <chrisreading.email@gmail.com>
2019-11-18 21:57:42 -0700

    Removed Game class and seperated JFrame and JPanels. GamePanel now holds the game's core features and the Window class will tie in all panels used in the game.

--- src/wordscrambler/Window.java
+++ src/wordscrambler/Window.java
@@ -1,5 +1,30 @@
 package wordscrambler;
 
-public class Window {
-	// TODO
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+import wordscrambler.gui.GamePanel;
+
+/**
+ * Window class, handles creating and adding the various panels to be used
+ * @author Chris Reading
+ *
+ */
+public class Window extends JFrame {
+	
+	public static void main(String[] args) {
+		new Window();
+	}
+	
+	public Window() {
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(new Dimension(800, 600));
+		
+		// add GamePanel
+		this.setContentPane(new GamePanel());
+		
+		this.setVisible(true);
+	}
+	
 }
---
+++ src/wordscrambler/gui/GamePanel.java
@@ -0,0 +1,143 @@
+package wordscrambler.gui;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+import wordscrambler.level.LevelManager;
+
+/**
+ * GamePanel class, displays the game area
+ * 
+ * @author Chris Reading
+ * @author Cannon Rudd
+ *
+ */
+public class GamePanel extends JPanel {
+	
+	/**
+	 * LevelManager in order to switch levels, etc
+	 */
+	private LevelManager lm;
+	
+	/**
+	 * List<Label> that contains all the labels that will be filled in when a user clicks a button
+	 */
+	private List<JLabel> labels;
+	
+	/**
+	 * List<JButton> buttons that the user can click to fill in labels
+	 */
+	private List<JButton> buttons;
+	
+	public GamePanel() {
+		// setup the level
+		lm = new LevelManager();
+		setupBoxLabels();
+		setupCharButtons();
+		
+		// DEBUGGING
+		System.out.println("Level Word: " + lm.getCurrentLevel().getWordGenerator().getWord());
+		
+		JLabel lblWordmix = new JLabel("Word-Mix");
+		lblWordmix.setForeground(Color.WHITE);
+		lblWordmix.setBackground(Color.WHITE);
+		lblWordmix.setFont(new Font("Apple Chancery", Font.PLAIN, 25));
+		lblWordmix.setHorizontalAlignment(SwingConstants.CENTER);
+		this.add(lblWordmix, BorderLayout.NORTH);
+		
+		this.setBackground(new Color(0, 204, 153));
+		this.setLayout(new GridLayout(0, 1, 0, 0));
+
+		JPanel boxPanel = new JPanel();
+		boxPanel.setBackground(new Color(0, 204, 153));
+		this.add(boxPanel);
+		
+		JPanel charPanel = new JPanel();
+		charPanel.setBackground(new Color(0, 204, 153));
+		this.add(charPanel);
+		
+		// add labels to the boxPanel
+		for(JLabel label : labels) {
+			boxPanel.add(label);
+		}
+		
+		// add buttons to the charPanel
+		for(JButton button : buttons) {
+			charPanel.add(button);
+		}
+	}
+	
+	/**
+	 * Creates the labels that will be fill in using the size of the word in the Level
+	 */
+	private void setupBoxLabels() {
+		labels = new ArrayList<JLabel>();
+
+		for(int i = 0; i < lm.getCurrentLevel().getWordGenerator().getWord().length(); i++) {
+			JLabel label = new JLabel("", SwingConstants.CENTER);
+			label.setPreferredSize(new Dimension(40, 40));
+			label.setOpaque(true);
+			label.setBackground(Color.WHITE);
+			label.setForeground(Color.BLACK);
+			labels.add(label);
+		}
+	}
+	
+	/**
+	 * Creates the buttons based on the Level's word charArray
+	 */
+	public void setupCharButtons() {
+		buttons = new ArrayList<JButton>();
+		
+		List<Character> charArray = lm.getCurrentLevel().getWordGenerator().getCharArray();
+		
+		for(Character letter : charArray) {
+			JButton btn = new JButton(letter.toString());
+			btn.setPreferredSize(new Dimension(80, 80));
+			btn.setFocusPainted(false);
+			
+			btn.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					JLabel label = getLabelToFill(labels);
+					label.setText(btn.getText());
+					btn.setVisible(false);
+				}
+			});
+
+			buttons.add(btn);
+		}
+	}
+	
+	/**
+	 * Gets the next label to fill in, if there are no more labels to fill in then
+	 * we will check to see if the user got the right word.
+	 * 
+	 * @param labels List<JLabel> to iterate through
+	 * @return Returns the JLabel to manipulate
+	 */
+	public JLabel getLabelToFill(List<JLabel> labels) {
+		for(JLabel label : labels) {
+			if(label.getText().isEmpty()) {
+				return label;
+			}
+		}
+		
+		// TODO: if there are no more labels to fill in, check to see if the player wins this level
+		return null;
+	}
+	
+}
