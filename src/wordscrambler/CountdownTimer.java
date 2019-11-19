32ece61bf6465384eb5f3d24273075c80b8407b6
nomad23541 <chrisreading.email@gmail.com>
2019-11-18 19:21:44 -0700

    Added TestClass and changed countdowntimer

--- src/wordscrambler/CountdownTimer.java
+++ src/wordscrambler/CountdownTimer.java
@@ -8,6 +8,7 @@ package wordscrambler;
 public class CountdownTimer {
 	
 	private int countdown;
+	private String test;
 	
 	public CountdownTimer(int countdown) {
 		this.countdown = countdown;
---
+++ src/wordscrambler/TestClass.java
@@ -0,0 +1,5 @@
+package wordscrambler;
+
+public class TestClass {
+
+}
